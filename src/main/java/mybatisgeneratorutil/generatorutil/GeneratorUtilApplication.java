c474a53c4f8736e9701b2636342758b0080031d9
Joker <2635225112@qq.com>
2020-07-16 01:36:38 +0800

    表生成代码工具

---
+++ src/main/java/mybatisgeneratorutil/generatorutil/GeneratorUtilApplication.java
@@ -0,0 +1,13 @@
+package mybatisgeneratorutil.generatorutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mybatisgeneratorutil/generatorutil/GeneratorUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package mybatisgeneratorutil.generatorutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
